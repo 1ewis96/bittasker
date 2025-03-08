9b62e44cebb61b98e5dbe1c4b20c72d9dcfc2fa2
metafarmers-eu <metafarmers@proton.me>
2025-03-08 20:12:18 +0000

    page

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -4,9 +4,9 @@ const HeroSection = () => {
   return (
     <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-dark text-white">
       <div className="col-md-6 p-lg-5 mx-auto my-5">
-        <h1 className="display-3 fw-bold">Designed for engineers</h1>
+        <h1 className="display-3 fw-bold">MetaFarmers</h1>
         <h3 className="fw-normal text-light mb-3">
-          Build anything you want with Aperture
+         Explore & Navigate.
         </h3>
         <div className="d-flex gap-3 justify-content-center lead fw-normal">
           <a className="icon-link text-light" href="#">
@@ -16,7 +16,7 @@ const HeroSection = () => {
             </svg>
           </a>
           <a className="icon-link text-light" href="#">
-            Buy
+            Client
             <svg className="bi" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793L9.146 3.854a.5.5 0 1 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 1 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
             </svg>
