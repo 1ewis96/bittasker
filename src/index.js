d50cf993f5cb68b04dd346e74b1734d64472707c
metafarmers-eu <metafarmers@proton.me>
2025-03-08 17:45:57 +0000

    no change

--- src/index.js
+++ src/index.js
@@ -5,29 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Amplify from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_CdtrjKgCZ',
-    userPoolWebClientId: 'YOUR_APP_CLIENT_ID',
-    authenticationFlowType: 'USER_SRP_AUTH',
-    cookieStorage: {
-      domain: '.bittasker.xyz',
-      path: '/',
-      expires: 365,
-      secure: true,
-    },
-    oauth: {
-      domain: 'auth.bittasker.xyz',
-      scope: ['openid', 'profile', 'email'],
-      redirectSignIn: 'https://bittasker.xyz/',
-      redirectSignOut: 'https://bittasker.xyz/',
-      responseType: 'code',
-    },
-  },
-});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -36,6 +13,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 
 // If you want to start measuring performance in your app, pass a function
