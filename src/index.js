87a7259e31add46c094fbf475e22a1c41b8f6edf
metafarmers-eu <metafarmers@proton.me>
2025-03-08 17:43:22 +0000

    no change

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Amplify from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_CdtrjKgCZ',
+    userPoolWebClientId: 'YOUR_APP_CLIENT_ID',
+    authenticationFlowType: 'USER_SRP_AUTH',
+    cookieStorage: {
+      domain: '.bittasker.xyz',
+      path: '/',
+      expires: 365,
+      secure: true,
+    },
+    oauth: {
+      domain: 'auth.bittasker.xyz',
+      scope: ['openid', 'profile', 'email'],
+      redirectSignIn: 'https://bittasker.xyz/',
+      redirectSignOut: 'https://bittasker.xyz/',
+      responseType: 'code',
+    },
+  },
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,7 +37,9 @@ root.render(
   </React.StrictMode>
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
