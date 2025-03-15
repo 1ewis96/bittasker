e44f130bf56b7892bfee5a2fe563c80be9f64c35
metafarmers-eu <metafarmers@proton.me>
2025-03-15 13:27:06 +0000

    authentication

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ import CognitoCallbackClear from './auth/CognitoCallbackClear';
 
 function App() {
 	
-const auth = useAuth();
-	
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Error: {auth.error.message}</div>;
-  }
-	
    return (
     <Router>
       <Routes> 
--- src/auth/CognitoCallback.js
+++ src/auth/CognitoCallback.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
 
 // Custom hook to wrap your secure function with an authentication check
 const CognitoCallback = () => {
   const auth = useAuth(); // Get the auth context
 
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Error: {auth.error.message}</div>;
+  }
+	
+	
   // Simply return "Hello World" for now
   return <div>Create Session</div>;
 };
