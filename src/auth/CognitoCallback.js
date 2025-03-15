22eea70a586e5c9114c091190761d323a1a5f581
metafarmers-eu <metafarmers@proton.me>
2025-03-15 13:21:09 +0000

    authentication

--- src/auth/CognitoCallback.js
+++ src/auth/CognitoCallback.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useAuth } from "react-oidc-context";
 
 // Custom hook to wrap your secure function with an authentication check
-const CognitoCallback = (callback) => {
+const CognitoCallback = () => {
   const auth = useAuth(); // Get the auth context
 
   // Simply return "Hello World" for now
